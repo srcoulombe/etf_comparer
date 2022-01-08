91cf7c86c94018f7f1c30a6debebadada3cd070e
samy.coulombe <samy.coulombe@mail.mcgill.ca>
2022-01-07 20:45:54 -0500

    backend.py now allows a choice of dbms

--- backend.py
+++ backend.py
@@ -5,4 +5,14 @@ from typing import Iterable, Mapping, Union
 from tinydb_database_client import TinyDBDatabaseClient
 from sqlite3_database_client import SQLite3DatabaseClient
 
+def select_database(db_type: str) -> Union[TinyDBDatabaseClient, SQLite3DatabaseClient]:
+    db_type = db_type.lower()
+    assert db_type in ('tinydb','sqlite3'), \
+        f"db_type must be in ('tinydb','sqlite3'); got {db_type}"
+    if db_type == 'tinydb':
+        return TinyDBDatabaseClient()
+    elif db_type == 'sqlite3':
+        return SQLite3DatabaseClient()
+    else:
+        raise Exception("should not have gone here")
 
