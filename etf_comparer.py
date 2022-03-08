4fc6cbc9d3b19e38f0bb958b087478f30afbd3d4
Samy Coulombe <samy.coulombe@mail.mcgill.ca>
2022-03-08 01:00:55 -0500

    patched up version with expanding help text and data download

--- etf_comparer.py
+++ etf_comparer.py
@@ -1,5 +1,7 @@
 
 # standard library dependencies
+import logging
+import logging.config
 from io import BytesIO
 from typing import List
 
@@ -9,12 +11,27 @@ from streamlit_tags import st_tags
 
 # local dependencies
 from src.backend import select_database
+from src.utils import get_etf_holding_weight_vectors
 from src.plotting import plot_holdings_tracks, plot_similarity
 
+logging.config.fileConfig("loggingConfig.yml")
+
+logging.info("Initiating Streamlit app")
 
 st.set_page_config(layout='centered')
 
 st.title('Comparing ETFs')
+
+with st.expander("What's an ETF?"):
+    st.markdown("""An **Exchange-Traded Fund (ETF)** is basically a variety-pack of other financial assets that you
+can purchase exactly as you purchase/invest in other stocks.
+    
+ETFs are often themed (i.e., they can be a collection of stocks from the same industry, location, etc..), 
+and some aim to track an index (e.g., the 'SPY' ETF aims to hold the same stocks listed in Standard & Poor's (S&P) 500 index).
+    
+ETFs have other advantages, such as allowing you to invest in assets that are otherwise difficult to access, 
+but **the main takeaway is that an ETF is a collection of assets (formally referred to as 'holdings') that are bundled together**.""")
+
 
 backend_option = st.selectbox(
     'Choose the database management system to use in the back end:',
@@ -23,6 +40,7 @@ backend_option = st.selectbox(
 st.write('Using:', backend_option)
 
 dbc = select_database(backend_option)
+logging.info(f"Connected to {backend_option} client instance")
 
 @st.cache 
 def clean_user_data(user_input: List[str]) -> List[str]:    
@@ -33,16 +51,23 @@ def clean_user_data(user_input: List[str]) -> List[str]:
     ]
 
 def run(user_input: str) -> None:
-    print('Loading data...')
-    print(user_input)
+    logging.info(f'Loading data for: {user_input}')
     etfs_data, unavailable_etfs = dbc.get_holdings_and_weights_for_etfs(
         clean_user_data(user_input)[:10]
     )
     if len(unavailable_etfs) > 0:
-        st.warning(f"Failed to fetch data for the following ETFs: {', '.join(unavailable_etfs)}")
-    print('Loaded data.')
-    print('Processing data...')
+        warning = f"Failed to fetch data for the following ETFs: {', '.join(unavailable_etfs)}"
+        logging.warning(warning)
+        st.warning(warning)
+
+    logging.info(f'Loaded data for: {user_input}')
+    logging.info(f'Processing data for: {user_input}')
+    
     st.pyplot(plot_holdings_tracks(etfs_data), dpi=1000)
+
+    logging.info(f'Processed data for: {user_input}')
+
+    logging.info(f'Calculating similarities between: {user_input}')
 
     # plot the similarity matrices
     col1, col2 = st.columns([5,5])
@@ -54,7 +79,7 @@ def run(user_input: str) -> None:
         # plot the similarity matrix
         fig = plot_similarity(etfs_data, distance_measure='weighted_jaccard')
         buf = BytesIO()
-        fig.savefig(buf, format="png", figsize=(4,4))
+        fig.savefig(buf, format="png")#, figsize=(4,4))
         st.image(buf)
     with col2:
         st.markdown(
@@ -64,10 +89,31 @@ def run(user_input: str) -> None:
         # plot the similarity matrix
         fig = plot_similarity(etfs_data, distance_measure='jaccard')
         buf = BytesIO()
-        fig.savefig(buf, format="png", figsize=(4,4))
+        fig.savefig(buf, format="png")#, figsize=(4,4))
         st.image(buf)
-
-    #st.pyplot(plot_similarity(etfs_data), figsize=(2,2), dpi=1000)
+    
+    logging.info(f'Calculated similarities between: {user_input}')
+    logging.info(f'Re-fetching data for: {user_input}')
+    logging.info(f'Calculated similarities between: {user_input}')
+    logging.info(f'Re-fetching data for: {user_input}')
+    data = get_etf_holding_weight_vectors(
+        etfs_data, 
+        as_df = True
+    )
+    logging.info(f'Re-fetched data for: {user_input}')
+    
+    st.subheader("Data")
+    with st.expander("Show Data"):
+        st.markdown("""The following data indicates the weight given to each holding in the chosen ETFs.""")
+        st.download_button(
+            "Press to Download",
+            data.to_csv().encode('utf-8'),
+            "file.csv",
+            "text/csv",
+            key='download-csv'
+        )
+        st.dataframe(data)
+    
 st.subheader("Specify up to 10 ETFs to compare")
 
 with st.form(key='my_form'):
