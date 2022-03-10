dca73327194d955bfcb84765e263f2e43eaeb859
Samy Coulombe <samy.coulombe@mail.mcgill.ca>
2022-03-09 20:24:07 -0500

    added print statements saying which scraper is being used

--- src/scraping/__init__.py
+++ src/scraping/__init__.py
@@ -10,24 +10,44 @@ from .invesco_scraper import FUNDS as invesco_etf_tickers
 from .invesco_scraper import fetch as fetch_from_invesco
 from .zack_scraper import fetch as fetch_from_zack
 
-ishares_etf_tickers = [etf.lower() for etf in ishares_etf_tickers]
-ark_etf_tickers = [etf.lower() for etf in ark_etf_tickers]
-invesco_etf_tickers = [etf.lower() for etf in invesco_etf_tickers]
+ishares_etf_tickers = [etf.upper() for etf in ishares_etf_tickers]
+ark_etf_tickers = [etf.upper() for etf in ark_etf_tickers]
+invesco_etf_tickers = [etf.upper() for etf in invesco_etf_tickers]
 
 def scrape_etf_holdings(etf: str) -> Mapping[str, Mapping[str, float]]:
-    etf = etf.lower()
+    """Entrypoint function to iterate over scrapers one-by-one
+    until one of them succeeds.
+
+    Parameters
+    ----------
+    etf : str
+        Ticker for the ETF of interest
+
+    Returns
+    -------
+    Mapping[str, Mapping[str, float]]
+        Either an empty dictionary (if no data on the specified ETF was available on zacks.com),
+        or a dictionary mapping a holding ticker (string) to a sub-dictionary mapping
+        'weight' to the holding ticker's weight in the ETF.
+
+    """
+    etf = etf.upper()
     source = "zack"
     try:
         if etf in ishares_etf_tickers:
+            print("Using ishares scraper")
             source = "ishares"
             etf_holdings_and_weights = fetch_from_ishares(etf)
         elif etf in ark_etf_tickers:
+            print("Using ark scraper")
             source = "ark"
             etf_holdings_and_weights = fetch_from_ark(etf)
         elif etf in invesco_etf_tickers:
+            print("Using invesco scraper")
             source = "invesco"
             etf_holdings_and_weights = fetch_from_invesco(etf)
         else:
+            print("Using zacks.com scraper")
             etf_holdings_and_weights = fetch_from_zack(etf)
     except Exception as e:
         print(f"getting updated holdings for ETF: {etf} raised {e}")
