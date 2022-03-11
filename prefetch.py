9c6e07c7b869b3bbc8f84a288d121872274faa89
Ubuntu <ubuntu@ip-172-31-93-1.ec2.internal>
2022-03-11 03:24:30 +0000

    added function and modularization

--- prefetch.py
+++ prefetch.py
@@ -2,7 +2,7 @@
 
 # standard library dependencies
 import time
-from datetime import datetime
+from datetime import datetime, date
 from concurrent.futures import ThreadPoolExecutor, as_completed
 # external dependencies
 
@@ -21,24 +21,33 @@ def prefetch_etf_data(etf: str) -> bool:
         print(f"Successfully pre-fetched data for {etf}")
         return True
 
-last_update = datetime.now().day
-while True:
-    day = datetime.now().day
-    print(last_update, day)
-    if day != last_update:
-        last_update = day
-        # get known etfs
-        pdc = PostgresDatabaseClient("aws_credentials.json")
-        known_etfs = pdc.get_known_etfs()
-        print(f"Pre-fetching data for {len(known_etfs)} ETFs")
-        with ThreadPoolExecutor(max_workers=8) as pool:
-            future_to_outcome = {pool.submit(prefetch_etf_data, etf): etf for etf in known_etfs}
-            for future in as_completed(future_to_outcome):
-                etf = future_to_outcome[future]
-                try:
-                    data = future.result()
-                except Exception as e:
-                    print(f'Pre-fetching data for {etf} generated an exception: {e}')
-                else:
-                    print(f'Successfully pre-fetched data for {etf}')
-    time.sleep(int(60*60*24))
+def get_latest_update() -> date:
+    print(f"Getting latest update date")
+    pdc = PostgresDatabaseClient("aws_credentials.json")
+    latest = pdc.execute_query("SELECT MAX(Date) FROM etf_holdings_table;")[0][0]
+    print(f"Latest update date: {latest}")
+    return latest
+
+if __name__ == '__main__':
+
+    last_update = get_latest_update()
+    while True:
+        day = datetime.now().day
+        print(last_update, day)
+        if day != last_update:
+            last_update = day
+            # get known etfs
+            pdc = PostgresDatabaseClient("aws_credentials.json")
+            known_etfs = pdc.get_known_etfs()
+            print(f"Pre-fetching data for {len(known_etfs)} ETFs")
+            with ThreadPoolExecutor(max_workers=8) as pool:
+                future_to_outcome = {pool.submit(prefetch_etf_data, etf): etf for etf in known_etfs}
+                for future in as_completed(future_to_outcome):
+                    etf = future_to_outcome[future]
+                    try:
+                        data = future.result()
+                    except Exception as e:
+                        print(f'Pre-fetching data for {etf} generated an exception: {e}')
+                    else:
+                        print(f'Successfully pre-fetched data for {etf}')
+        time.sleep(int(60*60))
